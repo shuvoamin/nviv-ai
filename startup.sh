0e8c6752f4c37e15ea3f7c347b8d0a49e95f9b20
shuvoamin <shuvoamin@yahoo.com>
2026-02-15 22:28:49 +0000

    Fix ModuleNotFoundError by activating virtual environment

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Direct path to the module since we are in the root
-python3 -m uvicorn backend.src.api:app --host 0.0.0.0 --port ${PORT:-8000}
+# Activate the virtual environment provided by Azure Oryx
+if [ -d "/home/site/wwwroot/antenv" ]; then
+    source /home/site/wwwroot/antenv/bin/activate
+    echo "Activated virtual environment 'antenv'"
+fi
+
+# Run the application
+python -m uvicorn backend.src.api:app --host 0.0.0.0 --port ${PORT:-8000}
